ff5796ea77ed0c122850d04d7d21147d862b3799
Jameem <jameemkurikkal.mp@gmail.com>
2024-04-04 17:53:00 +0200

    fix: cleanup

--- src/components/canvas/Ball.tsx
+++ src/components/canvas/Ball.tsx
@@ -15,6 +15,11 @@ interface BallProps {
   map: string;
   imgUrl: string;
 }
+
+interface BallCanvasProps {
+  map: string;
+  icon: string;
+}
 const Ball = (props: BallProps) => {
   const [map] = useLoader(TextureLoader, [props.map]);
   const [decal] = useTexture([props.imgUrl]);
@@ -23,15 +28,13 @@ const Ball = (props: BallProps) => {
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
-      <mesh castShadow receiveShadow scale={2.75}>
+      <mesh scale={2.75}>
         <sphereGeometry args={[1, 32, 32]} />
         <Decal
           position={[0, 0, 1]}
           rotation={[2 * Math.PI, 0, 6.25]}
           scale={1}
           map={decal}
-          // @ts-expect-error needed for shading
-          flatShading={true}
         />
         <meshPhongMaterial map={map} />
       </mesh>
@@ -39,13 +42,9 @@ const Ball = (props: BallProps) => {
   );
 };
 
-export const BallCanvas = ({ icon, map }: { map: string; icon: string }) => {
+export const BallCanvas = ({ icon, map }: BallCanvasProps) => {
   return (
-    <Canvas
-      frameloop='demand'
-      dpr={[1, 2]}
-      gl={{ preserveDrawingBuffer: true }}
-    >
+    <Canvas dpr={[1, 2]}>
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} />
         <Ball imgUrl={icon} map={map} />
